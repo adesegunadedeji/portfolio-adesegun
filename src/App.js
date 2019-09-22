f4a4377ed1551a5eda2a41baeb4cf8cf3c17a8fa
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-09-22 19:08:55 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <h1>Adesegun Adedeji</h1>
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Navbar} from 'react-bootstrap'
+function Navbar () {
+    return (
+        <div className = "Navbar_Container">
+            <Navbar bg="light">
+
+            </Navbar>
+
+        </div>
+    )
+
+}
+
+export default Navbar
