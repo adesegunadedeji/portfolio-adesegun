d3102859e49afecd9da314bb2d6193ca076c6b7f
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-09-22 19:36:25 -0400

    Navbar with Logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 import {Navbar} from 'react-bootstrap'
 function Navbar () {
     return (
-        <div className = "Navbar_Container">
-            <Navbar bg="light">
-
-            </Navbar>
-
-        </div>
-    )
-
-}
+    <div className = "Navbar_Container">
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+                <img alt="" src="/logo.svg" width="30"height="30" className="d-inline-block align-top"/>
+                {' Ade Adedeji'}
+            </Navbar.Brand>
+        </Navbar>
+    </div>
+    
+    )}
 
 export default Navbar
