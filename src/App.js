62cde7a1a45233c997ebc4d334417cd3421a856f
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-11-06 22:15:38 -0500

    Added comment

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
     <div className="App">
       <main>
         <Switch>
-       < Route path ='/about' component ={About}/>
-       < Route path ='/projects' component ={Projects}/>
-       < Route path ='/resume' component ={Resume}/>
-       < Route path ='/contact' component ={Contact}/> 
-       < Route path ='/' component ={Landing}/> 
+       < Route exact path ='/about' component ={About}/>
+       < Route  exact path ='/projects' component ={Projects}/>
+       < Route  exact path ='/resume' component ={Resume}/>
+       < Route  exact path ='/contact' component ={Contact}/> 
+       < Route  exact path ='/' component ={Landing}/> 
         <Redirect to='/'/>
        </Switch>
        </main>
