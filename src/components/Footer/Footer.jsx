fba488335b9669d551f636d4ad250375097c1060
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-11-16 19:19:44 -0500

    Change Website look temp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 function FooterComponent (){
         return (
             <div className = "Footerdiv">
               <nav className="w-100 centerNav textNav w-50-ns">
                 <div className="third">
-                <a  className="f6" href = "/">home</a>
+                <Link className="f6" to = "/">home</Link>
                 </div>
                 <div className="third">
-                <a  className="f6" href = "/projects">portfolio</a>
+                <Link className="f6" to = "/projects">portfolio</Link>
                 </div>
                 <div className="third">
-                <a  className="f6" href = "/about">contact</a>
+                <Link className="f6" to = "/about">about</Link>
                 </div>
                
                 </nav>
